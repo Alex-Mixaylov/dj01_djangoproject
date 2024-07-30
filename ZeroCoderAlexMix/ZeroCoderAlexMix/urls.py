10dc1211db2684df04c8c851bb3b80c45205a064
Alex <guardamar81@gmail.com>
2024-07-30 21:16:15 +0400

    update urls

--- ZeroCoderAlexMix/ZeroCoderAlexMix/urls.py
+++ ZeroCoderAlexMix/ZeroCoderAlexMix/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('main/', permanent=True)),  # Перенаправление на main/
     path('main/', include('main.urls')),  # Маршруты для main с префиксом main/
     path('dz01_dj01/', include('dz01_dj01.urls')),  # Маршруты для dz01_dj01 с префиксом dz01_dj01/
 ]
